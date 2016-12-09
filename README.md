01f02be4f10a2609d312039db7a591e058b8552a
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-12-09 19:54:01 +0100

    Add readme, change project name, add maven exec goal

---
+++ README.md
@@ -0,0 +1,14 @@
+# Spring experiments 
+
+With Thymeleaf template engine. 
+
+http://www.thymeleaf.org/
+
+https://projects.spring.io/spring-boot/
+
+## Usage
+
+    $ git clone https://github.com/remipassmoilesel/spring-experiments
+    $ mvn exec:java
+    
+    
--- src/main/java/org/remipassmoilesel/app/Application.java
+++ src/main/java/org/remipassmoilesel/app/Application.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
-        //System.setProperty("spring.template.cache", "false");
-
         SpringApplication.run(Application.class, args);
     }
 }
