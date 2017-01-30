178bca42e4517c015f502f69b1e895b1b289b99b
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-01-30 17:39:51 +0100

    Add tokens to form validation in order to prevent double validation

--- src/main/java/org/remipassmoilesel/MainController.java
+++ src/main/java/org/remipassmoilesel/MainController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -22,6 +25,8 @@ import java.util.Date;
 public class MainController {
 
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    private static final String BOOK_FORM_TOKEN = "book-form-token";
 
     @Autowired
     private ReservationService reservationService;
@@ -39,7 +44,7 @@ public class MainController {
     }
 
     @RequestMapping(value = Mappings.RESERVATION_LASTS, method = RequestMethod.GET)
-    public String showLastsReservation(Model model) {
+    public String showLastsReservation(HttpServletRequest request, Model model) {
 
         try {
             model.addAttribute("reservations", reservationService.getLasts(10));
@@ -52,15 +57,6 @@ public class MainController {
         return "last-reservations";
     }
 
-    @RequestMapping(value = Mappings.RESERVATION_FORM, method = RequestMethod.GET)
-    public String showReservationForm(Model model) {
-
-        //model.addAttribute("name", name);
-
-        // name of template
-        return "book-form";
-    }
-
     @RequestMapping(value = Mappings.RESERVATION_CALENDAR, method = RequestMethod.GET)
     public String showCalendar(Model model) {
 
@@ -70,8 +66,30 @@ public class MainController {
         return "calendar";
     }
 
+    @RequestMapping(value = Mappings.RESERVATION_FORM, method = RequestMethod.GET)
+    public String showReservationForm(
+            HttpServletRequest request,
+            Model model) {
+
+        // set request token
+        long token = System.currentTimeMillis();
+
+        // add it to model, to transmit it by forms
+        model.addAttribute("token", token);
+
+        // add it to session for check
+        HttpSession session = request.getSession();
+        session.setAttribute(BOOK_FORM_TOKEN, token);
+
+        // name of template
+        return "book-form";
+    }
+
     @RequestMapping(value = Mappings.DO_RESERVATION, method = RequestMethod.GET)
     public String doReservation(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam(value = "token", required = true) String token,
             @RequestParam(value = "firstname", required = true) String firstname,
             @RequestParam(value = "lastname", required = true) String lastname,
             @RequestParam(value = "phonenumber", required = true) String phonenumber,
@@ -82,22 +100,44 @@ public class MainController {
         String errorMessage = "";
         Customer customer = null;
         Reservation reservation = null;
-        try {
-            customer = customerService.createClient(firstname, lastname, phonenumber);
 
-            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-            Date departureDate = formatter.parse(departure);
-            Date arrivalDate = formatter.parse(departure);
-            reservation = reservationService.createReservation(customer, departureDate, arrivalDate);
+        // checks tokens
+        HttpSession session = request.getSession();
 
-        } catch (Exception e) {
-            logger.error("Error while parsing dates: " + departure, e);
-            errorMessage = e.getMessage();
+        //check tokens
+        Long sessionToken = (Long) session.getAttribute(BOOK_FORM_TOKEN);
+
+        // token is invalid
+        if (sessionToken == null || sessionToken.equals(Long.valueOf(token)) == false) {
+            logger.error("Invalid token: " + token + " / " + sessionToken);
+            errorMessage = "Veuillez reprendre le formulaire";
+        }
+
+        // token is valid
+        else {
+
+            // always delete token before leave
+            session.setAttribute(BOOK_FORM_TOKEN, null);
+
+            try {
+                customer = customerService.createClient(firstname, lastname, phonenumber);
+
+                DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                Date departureDate = formatter.parse(departure);
+                Date arrivalDate = formatter.parse(arrival);
+                reservation = reservationService.createReservation(customer, departureDate, arrivalDate);
+
+            } catch (Exception e) {
+                logger.error("Error while parsing dates: " + departure, e);
+                errorMessage = e.getMessage();
+            }
         }
 
         model.addAttribute("customer", customer);
         model.addAttribute("reservation", reservation);
         model.addAttribute("errorMessage", errorMessage);
+        model.addAttribute("response", response);
+
 
         // name of template
         return "book-completed";
