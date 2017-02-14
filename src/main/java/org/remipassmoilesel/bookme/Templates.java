a2744e2bb29770926494e693b52246acfd87d25a
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-02-14 17:05:38 +0100

    Fix mappings paths

--- src/main/java/org/remipassmoilesel/bookme/Templates.java
+++ src/main/java/org/remipassmoilesel/bookme/Templates.java
@@ -8,21 +8,21 @@ public class Templates {
     public static final String MAIN_TEMPLATE = "main-template";
 
     public static final String PAGES_DIR = "pages/";
-    public static final String DASHBOARD = PAGES_DIR + "/dashboard";
-    public static final String METRICS = PAGES_DIR + "/metrics";
+    public static final String DASHBOARD = PAGES_DIR + "dashboard";
+    public static final String METRICS = PAGES_DIR + "metrics";
 
-    public static final String RESERVATION_CALENDAR = PAGES_DIR + "/reservation-calendar";
-    public static final String RESERVATION_COMPLETED = PAGES_DIR + "/reservation-completed";
-    public static final String RESERVATION_FORM = PAGES_DIR + "/reservation-form";
-    public static final String RESERVATIONS_LAST = PAGES_DIR + "/reservation-show-last";
+    public static final String RESERVATION_CALENDAR = PAGES_DIR + "reservation-calendar";
+    public static final String RESERVATION_COMPLETED = PAGES_DIR + "reservation-completed";
+    public static final String RESERVATION_FORM = PAGES_DIR + "reservation-form";
+    public static final String RESERVATIONS_LAST = PAGES_DIR + "reservation-show-last";
 
-    public static final String ROOM_COMPLETED = PAGES_DIR + "/room-completed";
-    public static final String ROOM_FORM = PAGES_DIR + "/room-form";
-    public static final String ROOM_SHOW = PAGES_DIR + "/room-show";
+    public static final String ROOM_COMPLETED = PAGES_DIR + "room-completed";
+    public static final String ROOM_FORM = PAGES_DIR + "room-form";
+    public static final String ROOM_SHOW = PAGES_DIR + "room-show";
 
-    public static final String MESSAGE_SHOW_ALL = PAGES_DIR + "/message-show-all";
-    public static final String MESSAGE_NEW = PAGES_DIR + "/message-new";
-    public static final String MESSAGE_COMPLETED = PAGES_DIR + "/message-completed";
+    public static final String MESSAGE_SHOW_ALL = PAGES_DIR + "message-show-all";
+    public static final String MESSAGE_NEW = PAGES_DIR + "message-new";
+    public static final String MESSAGE_COMPLETED = PAGES_DIR + "message-completed";
 
-    public static final String CUSTOMER_SHOW = PAGES_DIR + "/customer-show-all";
+    public static final String CUSTOMER_SHOW = PAGES_DIR + "customer-show-all";
 }
