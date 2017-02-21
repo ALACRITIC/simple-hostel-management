3f3e9096ae130e41feefce8cfd8c50b10a2801b9
remi.passmoilesel <remi.passmoilesel@gmail.com>
2017-02-21 11:43:58 +0100

    Add menu icons

--- src/main/java/org/remipassmoilesel/bookme/admin/TiledMenu.java
+++ src/main/java/org/remipassmoilesel/bookme/admin/TiledMenu.java
@@ -12,19 +12,19 @@ import java.util.List;
 public class TiledMenu {
 
     private static List<TiledMenuItem> row_reservation = Arrays.asList(
-            new TiledMenuItem("New reservation", Mappings.RESERVATIONS_FORM, "assets/img/tile_new-reservation.svg"),
-            new TiledMenuItem("Calendar", Mappings.RESERVATIONS_CALENDAR, "assets/img/tile_reservation-calendar.svg"),
-            new TiledMenuItem("Last reservations", Mappings.RESERVATIONS_LASTS, "assets/img/tile_reservation-lasts.svg")
+            new TiledMenuItem("New reservation", Mappings.RESERVATIONS_FORM, "/assets/img/tile_new-reservation.svg"),
+            new TiledMenuItem("Calendar", Mappings.RESERVATIONS_CALENDAR, "/assets/img/tile_reservation-calendar.svg"),
+            new TiledMenuItem("Last reservations", Mappings.RESERVATIONS_LASTS, "/assets/img/tile_reservation-lasts.svg")
     );
     private static List<TiledMenuItem> row_service = Arrays.asList(
-            new TiledMenuItem("New service", Mappings.SERVICES_FORM, null),
-            new TiledMenuItem("Calendar", Mappings.SERVICES_CALENDAR, null),
-            new TiledMenuItem("View services", Mappings.SERVICES_SHOW_LASTS, null)
+            new TiledMenuItem("New service", Mappings.SERVICES_FORM, "/assets/img/tile_new-service.svg"),
+            new TiledMenuItem("Calendar", Mappings.SERVICES_CALENDAR, "/assets/img/tile_service-calendar.svg"),
+            new TiledMenuItem("Last services", Mappings.SERVICES_SHOW_LASTS, "/assets/img/tile_reservation-lasts.svg")
     );
     private static List<TiledMenuItem> row_customer = Arrays.asList(
-            new TiledMenuItem("Search customers", Mappings.CUSTOMERS_SEARCH, null),
-            new TiledMenuItem("Next checkouts", Mappings.RESERVATIONS_NEXT_CHECKOUTS, null),
-            new TiledMenuItem("Export bill", Mappings.ADMINISTRATION_BILL_FORM, "assets/img/tile_dollar.svg")
+            new TiledMenuItem("Search customers", Mappings.CUSTOMERS_SEARCH, "/assets/img/tile_search-customer.svg"),
+            new TiledMenuItem("Next checkouts", Mappings.RESERVATIONS_NEXT_CHECKOUTS, "/assets/img/tile_next-checkouts.svg"),
+            new TiledMenuItem("Export bill", Mappings.ADMINISTRATION_BILL_FORM, "/assets/img/tile_bill.svg")
     );
     private static List<TiledMenuItem> row_messages = Arrays.asList(
             new TiledMenuItem("New message", Mappings.MESSAGES_FORM, null),
